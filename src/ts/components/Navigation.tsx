9124896b012ce0f3c9560d3baf13df2910686fa7
Joshua Hammer <jebelfor@gmail.com>
2023-05-27 21:15:00 -0500

    Temp remove Home nav

--- src/ts/components/Navigation.tsx
+++ src/ts/components/Navigation.tsx
@@ -9,7 +9,7 @@ function Navigation() {
     <header>
       <nav className="navbar-container">
         <ul className="nav-links buttons-container">
-          <li><Link to="/"><button className="button-arounder">Home</button></Link></li>
+          {/*<li><Link to="/"><button className="button-arounder">Home</button></Link></li>*/}
           <li><Link to="https://www.linkedin.com/in/joshua-hammer/" target="_blank"><button className="button-arounder">LinkedIn</button></Link></li>
           <li><Link to="mailto:jebelfor@gmail.com" target="_blank"><button className="button-arounder">Contact</button></Link></li>
         </ul>
