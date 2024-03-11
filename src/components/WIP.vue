c81f7b0ed97c37ae17d57710d548f2c7226b82cb
Joshua Hammer <jebelfor@gmail.com>
2024-03-10 23:08:24 -0500

    Created WIP Notif

---
+++ src/components/WIP.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id='wip' class="wip animate__animated animate__bounce">
+    <h4>!!Under Development!!</h4>
+  </div>
+</template>
+      
+  <script lang="ts">
+
+    export default {
+      components: { },
+      setup() {
+
+  
+      return {
+
+      }
+    }
+  }
+  </script>
+    
+<style scoped>
+
+.wip {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: auto;
+  background-color: rgb(177, 103, 14);
+}
+
+</style>./WIP.vue
