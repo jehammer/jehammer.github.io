313238d7b0b91e9fb7f427ae119af8caeb879617
Joshua Hammer <jebelfor@gmail.com>
2024-03-10 23:06:45 -0500

    Breakdown Page into Sections

---
+++ src/components/IntroSection.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id='intro' class="intro">
+    <h1>Joshua Hammer</h1>
+    <h2>Software Developer</h2>
+    <p>Backend Development is my game.</p>
+  </div>
+    <!--<div class="card">
+        <Splitter style="height: 300px" class="mb-5">
+            <SplitterPanel class="flex align-items-center justify-content-center"> Panel 1 </SplitterPanel>
+            <SplitterPanel class="flex align-items-center justify-content-center"> Panel 2 </SplitterPanel>
+        </Splitter>
+    </div>-->
+</template>
+      
+<script lang="ts">
+import Splitter from 'primevue/splitter';
+import SplitterPanel from 'primevue/splitterpanel';
+  export default {
+    components: { Splitter, SplitterPanel },
+    setup() {
+      return {
+      }
+    }
+  }
+</script>
+    
+<style scoped>
+  .intro {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin: auto;
+  }
+
+  .intro h1 {
+    margin-top: 0;
+  }
+</style>./IntroSection.vue
---
+++ src/components/ProjectSection.vue
@@ -0,0 +1,118 @@
+<template>
+  <div class="projects-title">
+    <h2>
+      Projects.
+    </h2>
+  </div>
+  <v-container>
+    <v-row align="center" justify="center">
+      <!--BOKBot-->
+      <v-col cols="auto">
+        <v-card variant="elevated" class="mx-auto" max-width="344" color="indigo">
+          <v-card-item>
+            <v-card-title>
+              BOKBot
+            </v-card-title>
+            <v-card-subtitle>
+              Python, Yaml, MongoDB, RabbitMQ
+            </v-card-subtitle>
+          </v-card-item>
+          <v-card-text class="text-caption">
+            Discord Bot designed to manage rosters of players for endgame content and provide other functions for user engagement.
+          </v-card-text>
+          <v-card-actions>
+            <v-chip clickable href="https://github.com/jehammer/BOKBot" target="_blank" class="d-flex align-center" color="white" variant="outlined">
+              <font-awesome-icon :icon="['fab', 'github']"/>
+            </v-chip>
+          </v-card-actions>
+        </v-card>
+      </v-col cols="auto">
+      <!--Bridge-->
+      <v-col cols="auto">
+        <v-card variant="elevated" class="mx-auto" max-width="344" color="indigo">
+          <v-card-item>
+            <v-card-title>
+              Bridge
+            </v-card-title>
+            <v-card-subtitle>
+              TypeScript, MongoDB, Express, RabbitMQ
+            </v-card-subtitle>
+          </v-card-item>
+          <v-card-text class="text-caption">
+            REST API designed to communicate with front-end website to allow for engagement with BOKBot by sending messages via RabbitMQ.
+          </v-card-text>
+          <v-card-actions>
+            <v-chip clickable href="https://github.com/Breath-Of-Kynareth/bridge" target="_blank" class="d-flex align-center" color="white" variant="outlined">
+              <font-awesome-icon :icon="['fab', 'github']"/>
+            </v-chip>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+      <!--BOK Website-->
+      <v-col cols="auto">
+        <v-card variant="elevated" class="mx-auto" max-width="344" color="indigo">
+          <v-card-item>
+            <v-card-title>
+              BOK Website
+            </v-card-title>
+            <v-card-subtitle>
+              TypeScript, Vue3, Axios
+            </v-card-subtitle>
+          </v-card-item>
+          <v-card-text class="text-caption">
+            Front-End Website written in TypeScript and Vue3 to allow for more advanced interfacing with BOKBot and its systems from users.
+          </v-card-text>
+          <v-card-actions>
+            <v-chip clickable href="https://github.com/Breath-Of-Kynareth/breath-of-kynareth.github.io" target="_blank" class="d-flex align-center" color="white" variant="outlined">
+              <font-awesome-icon :icon="['fab', 'github']"/>
+            </v-chip>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+      
+<script lang="ts">
+  import Card from 'primevue/card';
+
+  export default {
+    components: { Card, },
+    setup() {
+      return {
+
+
+      }
+    }
+  }
+</script>
+  
+<style scoped>
+  .projects-title {
+    display: flex;
+    justify-content: center;
+  }
+
+</style>./ProjectSection.vue
+
+
+        <!--<v-card variant="elevated" class="mx-auto" max-width="344" color="indigo">
+          <v-card-item>
+            <div>
+              <div class="text-overline mb-1">
+              </div>
+              <div class="text-h6 mb-1">
+                BOKBot
+              </div>
+              <div class="text-caption">
+                Discord Bot written using Python with yaml designed to manage rosters of players for endgame content and provide
+                other functions for user engagement.
+              </div>
+            </div>
+          </v-card-item>
+          <v-card-actions>
+            <v-chip clickable href="https://github.com/jehammer/BOKBot"target="_blank" class="d-flex align-center" color="white" variant="outlined">
+              <font-awesome-icon :icon="['fab', 'github']"/>
+            </v-chip>
+          </v-card-actions>
+        </v-card>-->
