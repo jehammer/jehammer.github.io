94d28f6a9c5cbe6f18265cd56dff3d0e59667826
Joshua Hammer <jebelfor@gmail.com>
2024-03-10 23:08:07 -0500

    Removed Projects Page

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from '../pages/HomePage.vue';
-import ProjectsPage from '../pages/ProjectPage.vue';
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomePage,
-  },
-  {
-    path: '/projects',
-    name: 'projects',
-    component: ProjectsPage,
   },
   {
     path: '/:catchAll(.*)',
